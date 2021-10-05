364000f37c4931a428515a8280cc2128287e5977
Calebe Rocha <angelo.calebe@edu.pucrs.br>
2021-10-04 23:29:42 -0300

    Rota de autenticação, refatoração

--- src/main/kotlin/com/pucrs/construcaosoftware/api/UsersController.kt
+++ src/main/kotlin/com/pucrs/construcaosoftware/api/UsersController.kt
@@ -1,5 +1,9 @@
 package com.pucrs.construcaosoftware.api
 
+import com.pucrs.construcaosoftware.dto.UserCreateDTO
+import com.pucrs.construcaosoftware.dto.UserDTO
+import com.pucrs.construcaosoftware.dto.UserPartialUpdateDTO
+import com.pucrs.construcaosoftware.dto.UserUpdateDTO
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.enums.Explode
@@ -8,13 +12,8 @@ import io.swagger.v3.oas.annotations.enums.ParameterStyle
 import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.parameters.RequestBody
-import org.keycloak.admin.client.CreatedResponseUtil
-import org.keycloak.admin.client.Keycloak
-import org.keycloak.representations.idm.CredentialRepresentation
-import org.keycloak.representations.idm.UserRepresentation
 import org.springdoc.core.annotations.RouterOperation
 import org.springdoc.core.annotations.RouterOperations
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.stereotype.Component
@@ -23,7 +22,7 @@ import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.reactive.function.server.*
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
-import javax.ws.rs.NotFoundException
+import com.pucrs.construcaosoftware.keycloak.KeycloakClient
 
 @Configuration
 class UsersController {
@@ -151,7 +150,7 @@ class UsersController {
         ),
     )
     @Bean
-    fun routes(usersService: UsersService, handler: UsersHandler): RouterFunction<ServerResponse> =
+    fun usersRoutes(usersService: UsersService, handler: UsersHandler): RouterFunction<ServerResponse> =
         RouterFunctions.route(RequestPredicates.POST("/users")) {
             handler.create(it).flatMap { u -> ServerResponse.ok().bodyValue(u) }
         }
@@ -210,118 +209,3 @@ class UsersService(private val keycloakClient: KeycloakClient) {
     fun delete(id: String) = keycloakClient.delete(id)
 }
 
-@Component
-class KeycloakClient(
-    private val keycloak: Keycloak,
-    @Value("\${keycloak.realm}") private val realm: String,
-    @Value("\${keycloak.roles}") private val roles: List<String>,
-) {
-
-    fun create(user: UserCreateDTO): Mono<UserDTO> {
-        val realmResource = keycloak.realm(realm)
-
-        val roleRepresentation = realmResource.roles().get(user.role).toRepresentation()
-
-        val cred = CredentialRepresentation()
-        cred.type = CredentialRepresentation.PASSWORD
-        cred.isTemporary = false
-        cred.value = user.password
-
-        val userRepresentation = UserRepresentation()
-        userRepresentation.isEnabled = true
-        userRepresentation.username = user.username
-        userRepresentation.email = user.email
-
-        val response = keycloak.realm(realm).users().create(userRepresentation)
-        val userId = CreatedResponseUtil.getCreatedId(response)
-
-        val userResource = realmResource.users().get(userId)
-        userResource.resetPassword(cred)
-        userResource.roles().realmLevel().add(listOf(roleRepresentation))
-
-        return Mono.just(UserDTO(userId, user.username, user.role, user.email))
-    }
-
-    fun list(): Flux<UserDTO> = Flux.fromIterable(
-        roles.flatMap { role ->
-            keycloak.realm(realm).roles().get(role).roleUserMembers.map {
-                UserDTO(it.id, it.username, role, it.email)
-            }
-        }
-    )
-
-    fun get(id: String): Mono<UserDTO> {
-        val user = keycloak.realm(realm).users().get(id)
-
-        return Mono.fromCallable {
-            val userRepresentation = user.toRepresentation()
-            val role = user.roles().realmLevel().listAll().filter { roles.contains(it.name) }.firstOrNull()
-                UserDTO(
-                    userRepresentation.id,
-                    userRepresentation.username,
-                    role?.name,
-                    userRepresentation.email
-                )
-            }
-            .onErrorResume (NotFoundException::class.java) { Mono.empty() }
-    }
-
-    fun update(id: String, dto: UserUpdateDTO): Mono<UserDTO> {
-        val realmResource = keycloak.realm(realm)
-        val userResource = realmResource.users().get(id)
-
-        val roleToAdd = realmResource.roles().get(dto.role).toRepresentation()
-        val userRepresentation = UserRepresentation()
-        userRepresentation.email = dto.email
-
-        userResource.update(userRepresentation)
-
-        val realmLevelRoles = userResource.roles().realmLevel()
-        val rolesToRemove = realmLevelRoles.listAll().filter { roles.contains(it.name) }
-        realmLevelRoles.remove(rolesToRemove)
-        realmLevelRoles.add(listOf(roleToAdd))
-        val currentRole = realmLevelRoles.listAll().filter { roles.contains(it.name) }.firstOrNull()
-
-        val userR = userResource.toRepresentation()
-        return Mono.just(UserDTO(userR.id, userR.username, currentRole?.name, userR.email))
-    }
-
-    fun updatePassword(id: String, user: UserPartialUpdateDTO): Mono<SuccessDTO> {
-        val userResource = keycloak.realm(realm).users().get(id)
-
-        val cred = CredentialRepresentation()
-        cred.type = CredentialRepresentation.PASSWORD
-        cred.isTemporary = false
-        cred.value = user.password
-
-        userResource.resetPassword(cred)
-
-        return Mono.just(SuccessDTO(true))
-    }
-
-    fun delete(id: String): Mono<SuccessDTO> =
-        Mono.fromCallable { keycloak.realm(realm).users().delete(id) }.then(Mono.just(SuccessDTO(true)))
-}
-
-data class UserDTO(
-    val id: String,
-    val username: String,
-    val role: String? = null,
-    val email: String? = null,
-)
-
-data class UserCreateDTO(
-    val username: String,
-    val role: String? = null,
-    val email: String? = null,
-    val password: String? = null
-)
-
-data class UserUpdateDTO(
-    val role: String? = null,
-    val email: String? = null,
-)
-
-data class UserPartialUpdateDTO(val password: String)
-
-data class SuccessDTO(val success: Boolean? = false)
